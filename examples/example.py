da380cba9a44cadd60600de31db5a7224d5beb37
Parth_Soni <parthsoni1607@gmail.com>
2024-04-20 22:31:26 +0530

    added files

---
+++ examples/example.py
@@ -0,0 +1,74 @@
+"""
+Example usage of RerF module.  
+
+Paths to dataset are relative from "Python" source directory.
+"""
+
+from multiprocessing import cpu_count
+
+import numpy as np
+
+from rerf.RerF import fastPredict, fastPredictPost, fastRerF
+
+datatype = "iris"
+# datatype = "mnist"
+
+if datatype == "iris":
+    datafile = "../packedForest/res/iris.csv"
+    label_col = 4
+elif datatype == "mnist":
+    datafile = "../packedForest/res/mnist.csv"
+    label_col = 0
+
+print("loading data...")
+X = np.genfromtxt(datafile, delimiter=",")
+print("data loaded")
+
+if datatype == "iris":
+    feat_data = X[:, 0:4]  # iris
+elif datatype == "mnist":
+    feat_data = X[:, 1:]  # mnist
+
+labels = X[:, label_col]
+
+# forest = fastRerF(
+#     CSVFile=datafile,
+#     Ycolumn=label_col,
+#     forestType="binnedBaseRerF",
+#     trees=500,
+#     numCores=cpu_count(),
+# )
+forest = fastRerF(
+    X=feat_data, Y=labels, forestType="binnedBaseRerF", trees=500, numCores=cpu_count()
+)
+
+forest.printParameters()
+
+# training predictions
+predictions = fastPredict(feat_data, forest)
+# print(predictions)
+
+# training posterior predictions probabilities
+post_pred = fastPredictPost(feat_data, forest)
+# print(post_pred)
+
+print("Error rate", np.mean(predictions != labels))
+
+print("loading test data...")
+
+if datatype == "iris":
+    data_fname = "../packedForest/res/iris.csv"  # iris
+elif datatype == "mnist":
+    data_fname = "../packedForest/res/mnist_test.csv"  # mnist
+test_X = np.genfromtxt(data_fname, delimiter=",")
+
+print("data loaded")
+
+if datatype == "iris":
+    test_data = test_X[:, 0:4]  # iris
+elif datatype == "mnist":
+    test_data = test_X[:, 1:]  # mnist
+
+test_pred = fastPredict(test_data, forest)
+
+print("Error rate", np.mean(test_pred != test_X[:, label_col]))
